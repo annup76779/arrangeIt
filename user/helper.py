bcbee662f29be982bc050d034b8e5542b1211ecb
annup76779 <annup76779@gmail.com>
2023-07-10 07:22:13 +0530

    get join code of org after login.

--- user/helper.py
+++ user/helper.py
@@ -3,12 +3,12 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
-
     return {
         'refresh': str(refresh),
         'access': str(refresh.access_token),
         'user_role': user.role,
         'expires_in': refresh.lifetime,
         "email": user.email,
-        "name": user.first_name
+        "name": user.first_name,
+        "join_code": user.join_code if user.role == 1 else ""
     }
